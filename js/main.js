b74f5ad963da796e607169ffd5ac1129e676be77
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-24 19:37:19 -0700

    Fisrt commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', () => {
+    document.getElementById('default-msg').hidden = true;
+    document.getElementById('mini-games').hidden = false;
+})
