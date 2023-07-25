8ec80de30296860d977ab1eb64c4c4e0503beb1d
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-24 19:57:05 -0700

    Modified

--- js/main.js
+++
@@ -1,6 +0,0 @@
-let loginBtn = document.getElementById('login-btn');
-
-loginBtn.addEventListener('click', () => {
-    document.getElementById('default-msg').hidden = true;
-    document.getElementById('mini-games').hidden = false;
-})
