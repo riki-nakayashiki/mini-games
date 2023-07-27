98142d4dce5c0f5e6eee038f4f0cd34da321288d
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-27 15:58:23 -0700

    Add comments by Alice

--- js/timer.js
+++ js/timer.js
@@ -2,28 +2,28 @@ document.getElementById("startButton").addEventListener("click", start)
 document.getElementById("stopButton").addEventListener("click", stop)
 document.getElementById("resetButton").addEventListener("click", reset)
 
-let counter;
+let counter; ///global scope
 
-function start() {
+function start() { ///named function
     counter = setInterval(timer, 1000);
-    let startButton = document.getElementById("startButton");
+    let startButton = document.getElementById("startButton"); ///limited scope variable
     startButton.disabled = true;
 }
-function stop() {
+function stop() { ///named function
     clearInterval(counter);
-    let startButton = document.getElementById("startButton");
+    let startButton = document.getElementById("startButton"); ///limited scope variable
     startButton.disabled = false;
 }
-function reset() {
+function reset() { ///named function
     stop();
     document.getElementById("timerNumber").innerHTML = 10;
 }
-function timer() {
-    let timer = document.getElementById("timerNumber").innerHTML;
-    timer = parseInt(timer); ///Built-in Functions
-    timer--;
+function timer() { ///named function
+    let timer = document.getElementById("timerNumber").innerHTML; ///limited scope variable
+    timer = parseInt(timer); ///built-in-function - in this example, parseInt converts the HTML text to a number
+    timer--; ///Arithmetic operator decrement
     document.getElementById("timerNumber").innerHTML = timer;
-    if (timer == 0) {
+    if (timer == 0) { ///Comparison operators equal to
         stop();
         livesIndicate();
     }
